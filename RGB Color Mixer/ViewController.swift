c1888a588e3c315cc63d0d055bb9457607ddeddc
Vadim <sellmaxx@icloud.com>
2022-04-19 01:26:39 +0300

    palette + first slider

--- RGB Color Mixer/ViewController.swift
+++ RGB Color Mixer/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorPalette: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorPalette.layer.cornerRadius = 10
+        colorPalette.backgroundColor = .red
     }
 
 
